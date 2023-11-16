1f4485373e9ca20e009d7d821931893e40ddb6f5
Thien Long Ngo <ngo.andrej@gmail.com>
2023-11-16 22:58:02 +0100

    Clean code

--- src/client/views/SneakerForm/SneakerForm.types.ts
+++ src/client/views/SneakerForm/SneakerForm.types.ts
@@ -2,9 +2,4 @@ export namespace SneakerFormTypes {
   export type StyledFormProps = {
     $scrollPosition: number;
   };
-  // export type SneakerFormProps = {
-  //   onCreateSneaker: (newSneaker: SneakerInput) => void;
-  //   onUpdateSneaker?: (sneakerId: string, updatedSneaker: SneakerInput) => void;
-  //   onDeleteSneaker?: (sneakerId: string) => void;
-  // };
 }
