e2dcb6f831babc47115d7c06513d6dcb5b0a1d47
Thien Long Ngo <ngo.andrej@gmail.com>
2023-11-16 18:49:14 +0100

    Fix sneakers not sorted after manipulating with sneakers

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -6,7 +6,7 @@ import {
   fetchSneakers,
   updateSneaker,
 } from "./api/endpoints";
-import { SneakerInput } from "./types/sneakers";
+import { Sneaker, SneakerInput } from "./types/sneakers";
 import SneakerCollection from "./views/SneakerCollection/SneakerCollection";
 import Container from "./components/Container/Container";
 import { state$ } from "./store/store";
@@ -14,6 +14,7 @@ import Error from "./components/Error/Error";
 import Loader from "./components/Loader/Loader";
 import { GlobalStyles } from "./globalStyles";
 import EmptyCollection from "./components/Empty/EmptyCollection/EmptyCollection";
+import { SneakerSort } from "./enums/sneakers";
 
 const LazySneakerForm = React.lazy(
   () => import("./views/SneakerForm/SneakerForm"),
@@ -27,8 +28,9 @@ const App: React.FC = () => {
     refetch,
   } = useQuery("sneakers", fetchSneakers);
   const isSneakerFormVisible = state$.UI.isSneakerFormVisible.get();
-
-  // enableReactTracking({ auto: true });
+  const searchValue = state$.UI.searchValue;
+  const searchSneakers = state$.searchSneakers;
+  const activeSort = state$.UI.activeSort.get();
 
   useEffect(() => {
     if (sneakers !== undefined && sneakers.length > 0) {
@@ -36,6 +38,42 @@ const App: React.FC = () => {
       state$.searchSneakers.set([...sneakers]);
     }
   }, [sneakers]);
+
+  useEffect(() => {
+    const sortedSneakers: Sneaker[] =
+      searchValue.get() !== undefined && searchValue.get() !== ""
+        ? [...state$.searchSneakers.get()]
+        : [...state$.sneakers.get()];
+    switch (activeSort) {
+      case SneakerSort.yearUp:
+        sortedSneakers.sort((a, b) => a.year - b.year);
+        break;
+      case SneakerSort.yearDown:
+        sortedSneakers.sort((a, b) => b.year - a.year);
+        break;
+      case SneakerSort.sizeUp:
+        sortedSneakers.sort((a, b) => a.size - b.size);
+        break;
+      case SneakerSort.sizeDown:
+        sortedSneakers.sort((a, b) => b.size - a.size);
+        break;
+      case SneakerSort.priceUp:
+        sortedSneakers.sort((a, b) => a.price - b.price);
+        break;
+      case SneakerSort.priceDown:
+        sortedSneakers.sort((a, b) => b.price - a.price);
+        break;
+      default:
+        sortedSneakers.sort((a, b) => a.year - b.year);
+        break;
+    }
+
+    if (searchValue.get() !== undefined && searchValue.get() !== "") {
+      state$.searchSneakers.set(sortedSneakers);
+    } else {
+      state$.sneakers.set(sortedSneakers);
+    }
+  }, [activeSort, sneakers, searchSneakers]);
 
   useEffect(() => {
     isSneakerFormVisible
--- src/client/components/SearchForm/SearchForm.tsx
+++ src/client/components/SearchForm/SearchForm.tsx
@@ -2,14 +2,11 @@ import React, { ChangeEvent, useEffect } from "react";
 import { StyledSearchForm, StyledSearchFormInput } from "./SearchForm.styled";
 import Icon from "../Icon/Icon";
 import { state$ } from "../../store/store";
-import { Sneaker } from "../../types/sneakers";
-import { SneakerSort } from "../../enums/sneakers";
 
 const SearchForm: React.FC = () => {
-  const searchValue = state$.UI.searchValue;
   const sneakers = state$.sneakers;
+  const searchValue = state$.UI.searchValue;
   const searchSneakers = state$.searchSneakers;
-  const activeSort = state$.UI.activeSort.get();
 
   useEffect(() => {
     if (searchValue.get() !== "") {
@@ -27,42 +24,6 @@ const SearchForm: React.FC = () => {
       searchSneakers.set([...sneakers.get()]);
     }
   }, [searchValue.get()]);
-
-  useEffect(() => {
-    const sortedSneakers: Sneaker[] =
-      searchValue.get() !== undefined && searchValue.get() !== ""
-        ? [...state$.searchSneakers.get()]
-        : [...state$.sneakers.get()];
-    switch (activeSort) {
-      case SneakerSort.yearUp:
-        sortedSneakers.sort((a, b) => a.year - b.year);
-        break;
-      case SneakerSort.yearDown:
-        sortedSneakers.sort((a, b) => b.year - a.year);
-        break;
-      case SneakerSort.sizeUp:
-        sortedSneakers.sort((a, b) => a.size - b.size);
-        break;
-      case SneakerSort.sizeDown:
-        sortedSneakers.sort((a, b) => b.size - a.size);
-        break;
-      case SneakerSort.priceUp:
-        sortedSneakers.sort((a, b) => a.price - b.price);
-        break;
-      case SneakerSort.priceDown:
-        sortedSneakers.sort((a, b) => b.price - a.price);
-        break;
-      default:
-        sortedSneakers.sort((a, b) => a.year - b.year);
-        break;
-    }
-
-    if (searchValue.get() !== undefined && searchValue.get() !== "") {
-      state$.searchSneakers.set(sortedSneakers);
-    } else {
-      state$.sneakers.set(sortedSneakers);
-    }
-  }, [activeSort, sneakers, searchSneakers]);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     searchValue.set(e.target.value);
