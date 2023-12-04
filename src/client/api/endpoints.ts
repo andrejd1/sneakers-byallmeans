8924899df36e12b31ea821432a128a9c25c23023
Thien Long Ngo <ngo.andrej@gmail.com>
2023-12-04 12:28:32 +0100

    Add try catch blocks

--- src/client/api/endpoints.ts
+++ src/client/api/endpoints.ts
@@ -3,10 +3,16 @@ import { Sneaker, SneakerInput } from "../types/sneakers";
 import { extendedSeedData } from "../../server/seed";
 
 // export const fetchSneakers = async (): Promise<Sneaker[]> => {
-//   const response = await axios.get(
-//     `${process.env.VITE_LOCAL_API_BASE_URL}/sneakers`,
-//   );
-//   return response.data;
+//   try {
+//     const response = await axios.get(
+//       `${process.env.VITE_LOCAL_API_BASE_URL}/sneakers`,
+//     );
+//     return response.data;
+//   } catch (error) {
+//     // Handle the error, log it, throw a custom error, etc.
+//     console.error("Error fetching sneakers:", error);
+//     throw error; // Re-throwing the error so that the calling code can handle it.
+//   }
 // };
 
 //mock
@@ -17,27 +23,42 @@ export const fetchSneakers = async (): Promise<Sneaker[]> => {
 export const createSneaker = async (
   newSneaker: SneakerInput,
 ): Promise<Sneaker> => {
-  const response = await axios.post(
-    `${process.env.VITE_LOCAL_API_BASE_URL}/sneakers`,
-    newSneaker,
-  );
-  return response.data;
+  try {
+    const response = await axios.post(
+      `${process.env.VITE_LOCAL_API_BASE_URL}/sneakers`,
+      newSneaker,
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error creating sneaker:", error);
+    throw error;
+  }
 };
 
 export const updateSneaker = async (
   id: string,
   updatedSneaker: SneakerInput,
 ): Promise<Sneaker> => {
-  const response = await axios.put(
-    `${process.env.VITE_LOCAL_API_BASE_URL}/sneakers/${id}`,
-    updatedSneaker,
-  );
-  return response.data;
+  try {
+    const response = await axios.put(
+      `${process.env.VITE_LOCAL_API_BASE_URL}/sneakers/${id}`,
+      updatedSneaker,
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error updating sneaker:", error);
+    throw error;
+  }
 };
 
 export const deleteSneaker = async (id: string) => {
-  const response = await axios.delete(
-    `${process.env.VITE_LOCAL_API_BASE_URL}/sneakers/${id}`,
-  );
-  return response.data;
+  try {
+    const response = await axios.delete(
+      `${process.env.VITE_LOCAL_API_BASE_URL}/sneakers/${id}`,
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting sneaker:", error);
+    throw error;
+  }
 };
